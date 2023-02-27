b34d25b511d0afbcc6540294f514309ff6500a28
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-02-27 14:55:05 +0800

    Fixed, i tested it too.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+function log(a) {
+    console.log("[Modrinth Checklist] ", a)
+}
+
 URL = document.URL
-localStorage.setItem("mods-checklist-m","") 
+if (localStorage.getItem("mods-checklist-m","[]").indexOf("[]") > -1) {
+    localStorage.setItem("mods-checklist-m","[]")
+    log("s")
+}
+log(localStorage.getItem("mods-checklist-m","[]").indexOf("[]"))
 
 function waitForElm(selector) {
     return new Promise(resolve => {
@@ -21,10 +29,10 @@ function waitForElm(selector) {
     });
 }
 
-console.log(URL)
+log(URL)
 if (URL.startsWith("https://modrinth.com/checklist")) {
     document.querySelector("title").innerHTML = "Mod Checklist | Modrinth"
-    console.log("in url")
+    log("in url")
     main = document.querySelector("main")
     waitForElm('a[href="https://discord.gg/EUHuJHt"]').then((elm) => {
         elm.remove()
@@ -78,14 +86,14 @@ if (URL.startsWith("https://modrinth.com/checklist")) {
     download_button.classList.add("nuxt-link-active")
     download_button.innerHTML = "Download all mods"
     download_button.addEventListener("click", function() {
-        console.log("DOWNLOADING")
-        res = localStorage.getItem("mods-checklist-m").split(",")
-        console.log(res)
+        log("DOWNLOADING")
+        res = JSON.parse(localStorage.getItem("mods-checklist-m"))
+        log(res)
         for (let i = 0;i < res.length;i++) {
-            modid = res[i]
-            window.open(`https://modrinth.com/mod/${modid}/versions?g=${version_box.value}&l=${loader_box.value.toLowerCase()}${last}`, '_blank')
+            mod = res[i]
+            window.open(`${mod[1]}/versions?g=${version_box.value}&l=${loader_box.value.toLowerCase()}${last}`, '_blank')
         }
-        localStorage.setItem("mods-checklist-m", []) 
+        localStorage.setItem("mods-checklist-m", "[]")
     })
     download_button.style.marginLeft = "39%"
     const download_list = document.createElement("p")
@@ -96,15 +104,15 @@ if (URL.startsWith("https://modrinth.com/checklist")) {
     download_list.style.width = "100%"
     download_list.style.textAlign = "center"
     
-    res = localStorage.getItem("mods-checklist-m").split(",")
-    console.log(res)
+    res = JSON.parse(localStorage.getItem("mods-checklist-m"))
+    log(res)
     res.sort(function(a, b){
         return b.length - a.length;
     });
     final = base
     for (let i = 0;i < res.length;i++) {
         mod = res[i]
-        final = final + "<br>" + mod
+        final = final + "<br>" + mod[0] + " (" + mod[2] + ")"
     }
     download_list.innerHTML = final
 
@@ -117,20 +125,20 @@ if (URL.startsWith("https://modrinth.com/checklist")) {
 if (URL.includes("checklist=download")) {
     download = document.querySelector(".download-button")
     window.location = download.href
-    document.addEventListener("mousemove", function(){
-        window.close()   
-    })
+
 }
 
 document.addEventListener("mousemove", function(){
     waitForElm(".project-card").then(() => {
-        console.log("ae")
         if (document.querySelector(".project-card")) { 
             cards = document.querySelectorAll(".project-card")
             for (let i = 0;i < cards.length;i++) {
                 if (!cards[i].querySelector(".nuxt-link-active")) {
                     const checklist_add = document.createElement("button")
                     const nameofcard = cards[i].querySelector('h2[class="name"]').parentNode.href.replaceAll("https://modrinth.com/mod/","").replaceAll("https://modrinth.com/plugin/","").replaceAll("https://modrinth.com/datapack/","")
+                    const displayname = cards[i].querySelector('h2[class="name"]').innerHTML
+                    const cardlink = cards[i].querySelector('h2[class="name"]').parentNode.href
+                    const alldata = [displayname, cardlink, nameofcard]
                     if (localStorage.getItem("mods-checklist-m").indexOf(nameofcard) > -1) {
                         checklist_add.classList.add("brand-button")
                     }
@@ -142,22 +150,22 @@ document.addEventListener("mousemove", function(){
                         checklist_add.classList.toggle("brand-button")
                         let res = []
                         if (localStorage.getItem("mods-checklist-m")) {
-                            res = localStorage.getItem("mods-checklist-m").split(",")
-                            console.log(res)
+                            res = JSON.parse(localStorage.getItem("mods-checklist-m"))
+                            log(res)
                         }
                         if (checklist_add.classList.contains("brand-button")) {
-                            res.push(nameofcard)
-                            localStorage.setItem("mods-checklist-m", res.toString())
+                            res.push(alldata)
+                            localStorage.setItem("mods-checklist-m", JSON.stringify(res))
                             
-                            console.log("ADDING")
+                            log("ADDING")
                         } else {
-                            const index = res.indexOf(nameofcard);
+                            const index = res.indexOf(alldata);
                             if (index > -1) {
                                 res.splice(index, 1);
                             }
-                            localStorage.setItem("mods-checklist-m", res.toString())
+                            localStorage.setItem("mods-checklist-m", JSON.stringify(res))
                             
-                            console.log("REMOVING")
+                            log("REMOVING")
                         }
                     })
                     cards[i].appendChild(checklist_add)
